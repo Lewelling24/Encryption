556bfba95c575a78455de3029f6581b4454d9731
lewellingk24 <92475301+lewellingk24@users.noreply.github.com>
2022-03-31 20:14:50 -0400

    built out apply_Caesar method in Caesar_Cipher.java

--- src/Caesar_Cipher/Caesar_Cipher.java
+++ src/Caesar_Cipher/Caesar_Cipher.java
@@ -29,7 +29,35 @@ public class Caesar_Cipher {
     }
 
     // method to apply a Caesar shift to a given string
-    /*public String apply_Caesar(String text_to_encrypt, int shift_by) {
+    public String apply_Caesar(String text_to_encrypt, int shift_by) {
 
-    }*/
+        // use string builder to form string for return
+        StringBuilder encrypted_text = new StringBuilder();
+
+        // convert to character array for manipulation
+        char c_array [] = text_to_encrypt.toCharArray();
+
+        // for loop to iterate through character array
+        for(int i = 0; i < c_array.length; i++){
+
+            // check if character is a space
+            if(c_array[i] != ' ') {
+
+                int start_position = c_array[i] - 'a'; // subtract 'a' to account for ascii values >= 29
+                int end_position = (start_position + shift_by) % 26; // mod 26 to account for values >= 26
+
+                // type cast integer into character
+                char new_character = (char) ('a' + end_position); // add 'a' back for ascii values
+
+                encrypted_text.append(new_character);
+            }
+            else {
+                // if character is a space append it to encrypted_text string
+                encrypted_text.append(c_array[i]);
+            }
+        }
+
+        // convert string builder to string for return
+        return encrypted_text.toString();
+    }
 }
--- src/Driver/Driver.java
+++ src/Driver/Driver.java
@@ -1,9 +1,17 @@
 package Driver;
 
+
+import Caesar_Cipher.Caesar_Cipher;
+
 public class Driver {
 
     public static void main(String [] args) {
         //System.out.println("Hello World");
+
+        Caesar_Cipher cc = new Caesar_Cipher();
+
+        System.out.println(cc.apply_Caesar("i have a balloon", 14));
+
     }
 
 }
