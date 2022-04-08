e8bb009bb9957feed40c35881d6792656548734f
lewellingk24 <92475301+lewellingk24@users.noreply.github.com>
2022-04-08 17:10:18 -0400

    removed unused code

--- src/Caesar_Cipher/Caesar_Cipher.java
+++ src/Caesar_Cipher/Caesar_Cipher.java
@@ -2,31 +2,7 @@ package Caesar_Cipher;
 
 public class Caesar_Cipher {
 
-    // store string to be encrypted
-    private String text_to_encrypt;
 
-    // store integer by which to shift
-    private int shift_by;
-
-    // getter for integer to shift by
-    public int getShift_by() {
-        return shift_by;
-    }
-
-    // getter for text to be encrypted
-    public String getText_to_encrypt() {
-        return text_to_encrypt;
-    }
-
-    // setter for integer to shift by
-    public void setShift_by(int shift_by) {
-        this.shift_by = shift_by;
-    }
-
-    // setter for text to be encrypted
-    public void setText_to_encrypt(String text_to_encrypt) {
-        this.text_to_encrypt = text_to_encrypt;
-    }
 
     // method to apply a Caesar shift to a given string
     public static String apply_Caesar(String text_to_encrypt, int shift_by) {
